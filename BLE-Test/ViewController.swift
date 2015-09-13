461efd846b68de8a71abf4bafa61b39629673a31
Pierre Karashchuk <pierre@Pierres-MacBook-Pro.local>
2015-09-13 00:34:22 -0700

    Initial Commit

---
+++ BLE-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLE-Test
+//
+//  Created by Pierre Karashchuk on 9/13/15.
+//  Copyright (c) 2015 Pierre Karashchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
